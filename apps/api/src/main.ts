7a3d607b25f14c559cc0ee4beb3e2442de6bad6c
Smith Gajjar <smith.gajjar@gmail.com>
2020-11-28 18:41:06 +0530

    🔍 NewRelic Instance Added

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,2 +1,10 @@
+/**
+ * Imported `newrelic` for server monitoring.
+ */
+require('newrelic');
+
+/**
+ * Creating New Instance of the server
+ */
 import BootstrapApp from './app';
 new BootstrapApp();
